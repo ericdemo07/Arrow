a38db3465ca1221fc57b26423fe86903844e5c0c
Shukla <ayush.shukla@rakuten.com>
2016-02-10 18:19:54 +0530

    Response changes to productdetails

--- src/main/resources/static/productDetail/productDetail.controller.js
+++ src/main/resources/static/productDetail/productDetail.controller.js
@@ -1,20 +1,27 @@
-(function() 
-	{
-		angular.module('arrowApp').controller('ProductDetailController',ProductDetailController);
-		ProductDetailController.$inject = ['$scope','$stateParams', '$state'];
-		
-		function ProductDetailController($scope,$stateParams,$state) 
-		{
-			 $scope.submitProductDetails = function() 
-			 {	
-				 var productTypeSelected = $stateParams.productTypeSelected;
-				 var brandNameSelected = $stateParams.brandNameSelected;
-				 var modelNameSelected = $stateParams.modelNameSelected;
-				 var temp = this.vm;
-				 var productDetails = {productDescription:temp.productDescription,imageUpload1:temp.productImage1,
-						 imageUpload2:temp.productImage2,imageUpload3:temp.productImage1,invoiceDetails:""};
-				 console.log("Details "+productDetails);
-			 }
-		}	
+(function() {
+	angular.module('arrowApp').controller('ProductDetailController',
+			ProductDetailController);
+	ProductDetailController.$inject = [ '$scope', '$stateParams', '$state' ,'QueryService'];
+
+	function ProductDetailController($scope, $stateParams, $state, QueryService) {
+		$scope.submitProductDetails = function() {
+			console.log("Param : [ "+ $stateParams);
+			var productSelected = $stateParams;
+			var temp = this.vm;
+			var productDetails = {
+				productSelected : productSelected,	
+				productDescription : temp.productDescription,
+				imageUpload1 : temp.productImage1,
+				imageUpload2 : temp.productImage2,
+				imageUpload3 : temp.productImage1,
+				invoiceDetails : temp.productInvoiceUpload
+			};
+			console.log("Details " + productDetails);
+			QueryService.SendProductDetails(productTypeSelected).then(
+					function(response) {
+						$scope.selected = undefined;
+						$scope.brandNamesResponse = response;
+					});
+		}
 	}
-)();
+})();

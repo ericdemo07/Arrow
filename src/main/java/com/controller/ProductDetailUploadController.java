e37e9787fdff8140b58ca3b01618fa453f5dc4b6
ayush.shukla <ayush.shukla@RINPC211.intra.rakuten.co.jp>
2016-02-22 13:08:48 +0530

    set the destination for upload invoice file

--- src/main/java/com/controller/ProductDetailUploadController.java
+++ src/main/java/com/controller/ProductDetailUploadController.java
@@ -1,8 +1,15 @@
 package com.controller;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -15,14 +22,50 @@ import org.springframework.web.multipart.MultipartFile;
 public class ProductDetailUploadController 
 {
 	@RequestMapping(value = "/arrow/productdetailupload", method = RequestMethod.POST)
-	public @ResponseBody Object productDetailUploadQuery(@RequestParam("file") MultipartFile file, @RequestParam("username") String username) 
+	public @ResponseBody Object productDetailUploadQuery(@RequestParam("file") MultipartFile invoiceFile, @RequestParam("username") String username) 
 	{
 		MultipartFile a; 
 		System.out.println("[I am in SearchQueryController]");
 		List<String> brandName = new ArrayList<String>();
 
-		System.out.println("productInvoice : "+file.getSize());
+		File invoiceFileAsFile = new File("C:/E_Drive/Arrow/Dissect/bill1.pdf");
+		System.out.println("Content Type : "+"" );
+		try 
+		{
+			invoiceFile.transferTo(invoiceFileAsFile);
+		}
+		catch (IllegalStateException|IOException e1) 
+		{
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		
+		PDDocument document;
+
+		/*try 
+		{
+			document = PDDocument.load(new File("C:/E_Drive/Arrow/Dissect/bill.pdf"));
+		
+			System.out.println(document.getNumberOfPages());
+			for(PDPage page : document.getPages())
+			{
+				InputStream inputStream = page.getContents();
+		        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+		        StringBuilder out = new StringBuilder();
+		        String line;
+		        while ((line = reader.readLine()) != null) 
+		        {
+		            out.append(line);
+		        }
+		        System.out.println(out.toString());   //Prints the string content read from input stream
+		        reader.close();
+			}
+		} 
+		catch (IOException e) 
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}*/
 		return brandName.toArray();
 	}
-	
 }
